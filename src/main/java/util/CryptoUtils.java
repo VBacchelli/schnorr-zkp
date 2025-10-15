0d59878d76463be4283b5fa3f5f7661deed287d5
VBacchelli <valebacchelli@gmail.com>
2025-10-15 19:24:18 +0200

    better SecureRandom initialization

--- src/main/java/util/CryptoUtils.java
+++ src/main/java/util/CryptoUtils.java
@@ -4,7 +4,16 @@ import java.math.BigInteger;
 import java.security.SecureRandom;
 
 public final class CryptoUtils {
-    private static final SecureRandom RANDOM = new SecureRandom();
+	private static final SecureRandom RANDOM;
+    static { // static block, only runs once and initializes RANDOM
+        SecureRandom rnd;
+        try {
+            rnd = SecureRandom.getInstanceStrong(); // takes the best available instance
+        } catch (Exception e) {
+            rnd = new SecureRandom(); //fallback, default instance (system dependent)
+        }
+        RANDOM = rnd;
+    }
 
     private CryptoUtils() {}
 
